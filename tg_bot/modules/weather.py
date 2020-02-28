a229580d01e38d997d6cc0a1d15da472928589eb
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-02-28 22:03:57 +0530

    Update weather.py

--- tg_bot/modules/weather.py
+++ tg_bot/modules/weather.py
@@ -3,8 +3,8 @@ from pyowm import timeutils, exceptions
 from telegram import Message, Chat, Update, Bot
 from telegram.ext import run_async
 
-from haruka import dispatcher, updater, API_WEATHER
-from haruka.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher, updater, API_WEATHER
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
 @run_async
 def weather(bot, update, args):
