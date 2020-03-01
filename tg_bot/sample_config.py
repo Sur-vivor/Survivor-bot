4df88f051b721ea7aadd1885455e99e399502ebc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-03-01 23:19:42 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -12,7 +12,7 @@ class Config(object):
     # REQUIRED
     API_KEY = ""
     OWNER_ID = "765392045" # If you dont know, run the and do /id in pm
-    OWNER_USERNAME = Sarath
+    OWNER_USERNAME = "Sarath"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -26,6 +26,7 @@ class Config(object):
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    MAPS_API = ''
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
